8d1627e14a097064f129df621c2faa8bbf882290
Simeon Petrov <petrov.v.simeon@gmail.com>
2022-04-23 01:29:37 +0300

    feat: Main sections; Adjusted galleries; Added waves and primary colors

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+// FlexMasonry.init('.gallery', {
+//     responsive: true,
+//     breakpointCols: {
+//         'min-width: 1200px': 6,
+//         'min-width: 992px': 5,
+//         'min-width: 768px': 4,
+//         'min-width: 576px': 3,
+//         'min-width: 320px': 2,
+//     },
+// });
